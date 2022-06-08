4ca0d62109f184dd3c1098514c0da50c692e90e8
hyunkyungju <wngusrudrid@naver.com>
2022-06-08 10:46:01 +0900

    chore: 배포 테스트

--- backend/src/main/java/ybo/backend/domain/test/controller/TextResponseMessage.java
+++ backend/src/main/java/ybo/backend/domain/test/controller/TextResponseMessage.java
@@ -1,7 +1,7 @@
 package ybo.backend.domain.test.controller;
 
 public class TextResponseMessage {
-    public static final String COMMENT_SAVE_SUCCESS = "댓글 저장 성공";
+    public static final String COMMENT_SAVE_SUCCESS = "댓글 저장 성공 ";
     public static final String FIND_ALL_SUCCESS = "모든 댓글 읽어오기 성공";
 
 }
