6778a0c9546bedcf298c74ea7d6f8b7177169966
hyunkyungju <wngusrudrid@naver.com>
2022-06-09 13:04:42 +0900

    feat: spring 기본 설정을 local로 변경

--- backend/src/main/java/ybo/backend/BackendApplication.java
+++ backend/src/main/java/ybo/backend/BackendApplication.java
@@ -1,13 +1,31 @@
 package ybo.backend;
 
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(BackendApplication.class, args);
+		Test test = context.getBean(Test.class);
+		test.say();
 	}
 
+	@Component
+	class Test {
+		@Value("${spring.profiles.active}")
+		private String active;
+		public void say() {
+			System.out.println("*************************");
+			System.out.println("active: " + active);
+			System.out.println("*************************");
+		}
+	}
 }
