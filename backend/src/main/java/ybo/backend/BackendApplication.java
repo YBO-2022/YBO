304156bf79c5fe32dea80475bb5dd321f10e6bf9
hyunkyungju <wngusrudrid@naver.com>
2022-06-12 08:02:21 +0900

    feat: origin 출력 테스트

--- backend/src/main/java/ybo/backend/BackendApplication.java
+++ backend/src/main/java/ybo/backend/BackendApplication.java
@@ -1,7 +1,5 @@
 package ybo.backend;
 
-import org.hibernate.cfg.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,8 +9,7 @@ import org.springframework.stereotype.Component;
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args)  {
 		ApplicationContext context = SpringApplication.run(BackendApplication.class, args);
 		Test test = context.getBean(Test.class);
 		test.say();
--- backend/src/main/java/ybo/backend/domain/test/controller/TextController.java
+++ backend/src/main/java/ybo/backend/domain/test/controller/TextController.java
@@ -3,6 +3,7 @@ package ybo.backend.domain.test.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ybo.backend.domain.test.domain.Text;
@@ -13,6 +14,7 @@ import ybo.backend.global.error.exception.CustomIllegalArgumentException;
 import ybo.backend.global.response.DefaultResponse;
 import ybo.backend.global.response.StatusCode;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Slf4j
@@ -20,19 +22,19 @@ import java.util.List;
 @RestController
 // @RequestMapping("/api")
 public class TextController {
-
     private final TextService textService;
 
-    @PostMapping("/api/value")
-    public DefaultResponse saveText(@RequestBody TextSaveRequestDto textSaveRequestDto, BindingResult bindingResult) {
+    @PostMapping("/api/value") // 수정
+    public DefaultResponse saveText(HttpServletRequest request, @RequestBody TextSaveRequestDto textSaveRequestDto, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             throw new CustomIllegalArgumentException(GlobalErrorResponseMessage.ILLEGAL_ARGUMENT_ERROR, bindingResult);
         }
+        System.out.println("request.getHeader(HttpHeaders.ORIGIN); = " + request.getHeader(HttpHeaders.ORIGIN));
         Text savedText = textService.save(textSaveRequestDto.createEntityByDto());
         return DefaultResponse.res(StatusCode.OK, TextResponseMessage.COMMENT_SAVE_SUCCESS, savedText);
     }
 
-    @GetMapping("/api/values")
+    @GetMapping("/api/values")  // 수정 
     public DefaultResponse findAllTexts() {
         List<Text> texts = textService.findAll();
         return DefaultResponse.res(StatusCode.OK, TextResponseMessage.FIND_ALL_SUCCESS, texts);
