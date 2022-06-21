ba00bd05e4a9129857535c46316e039a4c36b7bb
hyunkyungju <wngusrudrid@naver.com>
2022-06-21 20:44:54 +0900

    feat: 스프링에서 flask로 API 요청

---
+++ backend/src/main/java/ybo/backend/domain/realtime/controller/RealTimeResponseMessage.java
@@ -0,0 +1,5 @@
+package ybo.backend.domain.realtime.controller;
+
+public class RealTimeResponseMessage {
+    public static final String REALTIME_RANKING_SEND_SUCCESS = "실시간 순위 데이터 전송 성공 ";
+}
---
+++ backend/src/main/java/ybo/backend/domain/realtime/dto/TeamRankDto.java
@@ -0,0 +1,13 @@
+package ybo.backend.domain.realtime.dto;
+
+public class TeamRankDto {
+    private Integer ranking;
+    private String team;
+    private String gameNumber;
+    private Integer winNumber;
+    private Integer loseNumber;
+    private Integer drawNumber;
+    private Float winRate;
+    private Float gameDiff;
+    private String continuity;
+}
