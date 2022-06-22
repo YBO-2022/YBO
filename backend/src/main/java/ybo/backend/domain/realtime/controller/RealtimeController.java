58cae3d90f1a135b1f36a4181a29d1c67c77a9d7
hyunkyungju <wngusrudrid@naver.com>
2022-06-22 17:06:30 +0900

    fix: 월요일 데이터를 404가 아닌 204로 전송

--- backend/src/main/java/ybo/backend/domain/realtime/controller/RealtimeController.java
+++ backend/src/main/java/ybo/backend/domain/realtime/controller/RealtimeController.java
@@ -47,7 +47,7 @@ public class RealtimeController {
         DefaultResponse response = restTemplate.getForObject(flaskServerUrl+apiUrl, DefaultResponse.class);
         int flaskStatusCode = response.getStatusCode();
         if (flaskStatusCode==StatusCode.NOT_FOUND){
-            return DefaultResponse.res(StatusCode.NOT_FOUND, response.getMessage());
+            return DefaultResponse.res(StatusCode.NO_CONTENT, response.getMessage());
         }
         List<RealtimeGameDto> data = (List<RealtimeGameDto>) response.getData();
         return DefaultResponse.res(StatusCode.OK, RealtimeResponseMessage.REALTIME_GAME_SEND_SUCCESS, data);
