9a7febcf4d40980538a62c5bf328093052154c46
hyunkyungju <93658689+hyunkyungju@users.noreply.github.com>
2022-06-22 10:35:45 +0900

    fix: 대문자 파일 삭제

--- backend/src/main/java/ybo/backend/domain/realtime/controller/RealTimeResponseMessage.java
+++
@@ -1,6 +0,0 @@
-package ybo.backend.domain.realtime.controller;
-
-public class RealtimeResponseMessage {
-    public static final String REALTIME_RANKING_SEND_SUCCESS = "실시간 순위 데이터 전송 성공 ";
-    public static final String REALTIME_GAME_SEND_SUCCESS = "실시간 게임 정보 데이터 전송 성공 ";
-}
