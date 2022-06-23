56d727f934bd391c89461a78b9c8419e4cdb498e
hyunkyungju <wngusrudrid@naver.com>
2022-06-23 14:25:54 +0900

    feat: 스프링이 켜질 때 실시간 랭킹 데이터 크롤링 수행

--- backend/src/main/java/ybo/backend/BackendApplication.java
+++ backend/src/main/java/ybo/backend/BackendApplication.java
@@ -1,20 +1,30 @@
 package ybo.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import ybo.backend.global.response.DefaultResponse;
 
 @SpringBootApplication
 @EnableWebMvc
 public class BackendApplication {
 
+	@Autowired
+	RestTemplate restTemplate;
+
+	@Value("${flask.server.url}")
+	private String flaskServerUrl;
+
 	public static void main(String[] args)  {
 		ApplicationContext context = SpringApplication.run(BackendApplication.class, args);
 		Test test = context.getBean(Test.class);
 		test.say();
+		test.crawling();
 	}
 
 	@Component
@@ -25,6 +35,9 @@ public class BackendApplication {
 			System.out.println("*************************");
 			System.out.println("active: " + active);
 			System.out.println("*************************");
+		}
+		public void crawling(){
+			restTemplate.getForObject(flaskServerUrl+"/rank", DefaultResponse.class);
 		}
 	}
 }
