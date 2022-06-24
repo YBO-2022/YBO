9c2f50a429e76064fc6482fddfe3baa0488ce26b
Jeong Hyunji <guswl3201@naver.com>
2022-06-25 03:48:02 +0900

    c

--- frontend/src/Components/GoldenGlove.js
+++ frontend/src/Components/GoldenGlove.js
@@ -14,12 +14,13 @@ function GoldenGlove() {
       <GGRanking id="0"/>
       <GGRanking id="2"/>
       <div className="glove_img">
-          <div className="g_one"><span style={{paddingRight: "15%"}}>좌익수</span> <span style={{paddingRight: "15%"}}>중견수</span> 우익수</div>
-          <div className="g_two"><span style={{paddingRight: "5%"}}>유격수</span> 2루수</div>
-          <div className="g_three"><span style={{paddingRight: "30%"}}>3루수</span> 1루수</div>
+          <div className="g_one"><span style={{paddingRight: "20%"}}>좌익수</span> <span style={{paddingRight: "20%"}}>중견수</span> 우익수</div>
+          <div className="g_two"><span style={{paddingRight: "3%"}}>유격수</span> 2루수</div>
+          <div className="g_three"><span style={{paddingRight: "31%"}}>3루수</span> 1루수</div>
           <div className="g_four">투수</div>
           <div className="g_five"><span style={{paddingRight: "10%"}}>포수</span> 지명타자</div>
       </div>
+      <br></br><br></br>
       </div>
     </div>
   );
