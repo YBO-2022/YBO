e735c11b3c0295853f8eb972e70ae0dcfbedee2f
hyunkyungju <wngusrudrid@naver.com>
2022-09-30 20:04:35 +0900

    fix: hitter id column명 추가

--- backend/src/main/java/ybo/backend/domain/players/domain/Hitter.java
+++ backend/src/main/java/ybo/backend/domain/players/domain/Hitter.java
@@ -15,6 +15,7 @@ import javax.persistence.*;
 public class Hitter {
     // 이름 팀 OPS 게임수 타석 타율 안타 홈런 타점 도루 삼진 병살 WAR
     @Id @GeneratedValue
+    @Column(name = "hitter_id")
     @JsonIgnore
     private Integer id;
     private String name;
