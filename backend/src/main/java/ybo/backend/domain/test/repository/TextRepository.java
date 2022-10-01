f4167575910d98b67a879904501ccff3245d3cf1
hyunkyungju <wngusrudrid@naver.com>
2022-10-01 20:34:05 +0900

    chore: 사용하지 않는 예시 코드 삭제

--- backend/src/main/java/ybo/backend/domain/test/repository/TextRepository.java
+++
@@ -1,9 +0,0 @@
-package ybo.backend.domain.test.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ybo.backend.domain.test.domain.Text;
-
-
-public interface TextRepository extends JpaRepository<Text, Long>{
-
-}
